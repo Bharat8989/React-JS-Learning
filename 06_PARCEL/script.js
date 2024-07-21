239d7234f8bc880c9d71701838aa506bde26e57e
Bharat <kadamb208@gmail.com>
2024-07-21 07:28:45 +0530

    adding files parcel

---
+++ 06_PARCEL/script.js
@@ -0,0 +1,10 @@
+// import {user} from './data.js'
+// import React from "react"
+// console.log(React)
+
+const h1=<h1>Hello world12</h1>
+console.log("hello world   ")
+
+if(module.hot){
+    module.hot.accept();
+}
