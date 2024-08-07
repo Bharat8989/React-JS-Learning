8e738048fe13952e60fbbb0ca277fee5021dc6ec
Bharat <kadamb208@gmail.com>
2024-08-07 13:50:52 +0530

    adding files

--- code-step-by-step/src/App.js
+++ code-step-by-step/src/App.js
@@ -1,18 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 // import User from './User';
 
 function App() {
-  let data="Bharat Kadam"
-  function Apple(){
-    alert("functions called ")
+  // let data="Bharat Kadam"
+  const [data, setData] = useState(0);
+
+  function Apple() {
+    // setData('Suraj');
+    setData(data+1)
+    // alert(setData);
   }
+
+  console.log("................");
   return (
     <div className="App">
       <h1>{data} !</h1>
-
-      <button onClick={Apple}>Click me</button>
-      <button onClick={()=>(alert("hello second click button"))}>Click me2</button>
+      <button onClick={Apple}>Update Data</button>
+      <button onClick={() => alert("hello second click button")}>Click me2</button>
       {/* <User/> */}
       {/* <User/>
       <User/>
@@ -20,7 +26,5 @@ function App() {
     </div>
   );
 }
-
-
 
 export default App;
