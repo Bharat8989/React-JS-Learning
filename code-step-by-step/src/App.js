eaf5d6c46aa5b5fc363798f65eac7ae56455957a
Bharat <kadamb208@gmail.com>
2024-08-07 13:09:44 +0530

    adding user fiels

--- code-step-by-step/src/App.js
+++ code-step-by-step/src/App.js
@@ -1,12 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+// import User from './User';
 
 function App() {
+  let data="Bharat Kadam"
+  function Apple(){
+    alert("functions called ")
+  }
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>{data} !</h1>
+
+      <button onClick={Apple}>Click me</button>
+      <button onClick={()=>(alert("hello second click button"))}>Click me2</button>
+      {/* <User/> */}
+      {/* <User/>
+      <User/>
+      <User/> */}
     </div>
   );
 }
+
+
 
 export default App;
