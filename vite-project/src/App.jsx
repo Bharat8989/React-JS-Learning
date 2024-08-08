b2c44c94d60669d37c12fa2aa8fc75003fc946c5
Bharat <kadamb208@gmail.com>
2024-08-08 20:30:10 +0530

    adding files app.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,15 +3,76 @@ import React from 'react'
 
 const App = () => {
   return (
+   <div>
     <div>
+     <div>
       <div>
-        <img src="https://th.bing.com/th/id/OIP.q_ncYnMDBTBP8Wr1eBtCggHaEK?rs=1&pid=ImgDetMain" width='100%' height='400px' alt="" />
+        <img src="https://th.bing.com/th/id/OIP.q_ncYnMDBTBP8Wr1eBtCggHaEK?rs=1&pid=ImgDetMain" width='70%' height='400px' alt="" />
       </div>
       <h2>Name:Queen of Tears</h2>
       <h3>Rating : 8.2</h3>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque minima, officia veritatis doloremque incidunt odit porro, hic harum error fugiat ducimus in? Ducimus minima cum nesciunt distinctio sint numquam ex.</p>
     </div>
+   </div>
+   <div>
+     <div>
+      <div>
+        <img src="https://th.bing.com/th/id/OIP.q_ncYnMDBTBP8Wr1eBtCggHaEK?rs=1&pid=ImgDetMain" width='70%' height='400px' alt="" />
+      </div>
+      <h2>Name:Queen of Tears</h2>
+      <h3>Rating : 8.2</h3>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque minima, officia veritatis doloremque incidunt odit porro, hic harum error fugiat ducimus in? Ducimus minima cum nesciunt distinctio sint numquam ex.</p>
+    </div>
+   </div>
+   <div>
+     <div>
+      <div>
+        <img src="https://th.bing.com/th/id/OIP.q_ncYnMDBTBP8Wr1eBtCggHaEK?rs=1&pid=ImgDetMain" width='70%' height='400px' alt="" />
+      </div>
+      <h2>Name:Queen of Tears</h2>
+      <h3>Rating : 8.2</h3>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque minima, officia veritatis doloremque incidunt odit porro, hic harum error fugiat ducimus in? Ducimus minima cum nesciunt distinctio sint numquam ex.</p>
+    </div>
+   </div>
+   <div>
+     <div>
+      <div>
+        <img src="https://th.bing.com/th/id/OIP.q_ncYnMDBTBP8Wr1eBtCggHaEK?rs=1&pid=ImgDetMain" width='70%' height='400px' alt="" />
+      </div>
+      <h2>Name:Queen of Tears</h2>
+      <h3>Rating : 8.2</h3>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque minima, officia veritatis doloremque incidunt odit porro, hic harum error fugiat ducimus in? Ducimus minima cum nesciunt distinctio sint numquam ex.</p>
+    </div>
+   </div>
+   <div>
+    <NetflixSeries></NetflixSeries>
+     <div>
+      <div>
+        <img src="https://th.bing.com/th/id/OIP.q_ncYnMDBTBP8Wr1eBtCggHaEK?rs=1&pid=ImgDetMain" width='70%' height='400px' alt="" />
+      </div>
+      <h2>Name:Queen of Tears</h2>
+      <h3>Rating : 8.2</h3>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque minima, officia veritatis doloremque incidunt odit porro, hic harum error fugiat ducimus in? Ducimus minima cum nesciunt distinctio sint numquam ex.</p>
+    </div>
+   </div>
+   </div>
+  )
+}
+
+const NetflixSeries=()=>{
+  return (
+    <div>
+     <div>
+      <div>
+        <img src="https://th.bing.com/th/id/OIP.q_ncYnMDBTBP8Wr1eBtCggHaEK?rs=1&pid=ImgDetMain" width='70%' height='400px' alt="" />
+      </div>
+      <h2>Name:Queen of Tears</h2>
+      <h3>Rating : 8.2</h3>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque minima, officia veritatis doloremque incidunt odit porro, hic harum error fugiat ducimus in? Ducimus minima cum nesciunt distinctio sint numquam ex.</p>
+    </div>
+   </div>
   )
 }
 
 export default App
+
