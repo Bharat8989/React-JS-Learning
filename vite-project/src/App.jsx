c6d1f128cdf03e8dcc0857206c3d1b4768bc6222
Bharat <kadamb208@gmail.com>
2024-08-08 22:38:09 +0530

    adding files for dynamic values in jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,73 +3,35 @@ import React from 'react'
 
 const App = () => {
   return (
-   <div>
-    <div>
-     <div>
-      <div>
-        <img src="https://th.bing.com/th/id/OIP.q_ncYnMDBTBP8Wr1eBtCggHaEK?rs=1&pid=ImgDetMain" width='70%' height='400px' alt="" />
-      </div>
-      <h2>Name:Queen of Tears</h2>
-      <h3>Rating : 8.2</h3>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque minima, officia veritatis doloremque incidunt odit porro, hic harum error fugiat ducimus in? Ducimus minima cum nesciunt distinctio sint numquam ex.</p>
-    </div>
-   </div>
-   <div>
-     <div>
-      <div>
-        <img src="https://th.bing.com/th/id/OIP.q_ncYnMDBTBP8Wr1eBtCggHaEK?rs=1&pid=ImgDetMain" width='70%' height='400px' alt="" />
-      </div>
-      <h2>Name:Queen of Tears</h2>
-      <h3>Rating : 8.2</h3>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque minima, officia veritatis doloremque incidunt odit porro, hic harum error fugiat ducimus in? Ducimus minima cum nesciunt distinctio sint numquam ex.</p>
-    </div>
-   </div>
-   <div>
-     <div>
-      <div>
-        <img src="https://th.bing.com/th/id/OIP.q_ncYnMDBTBP8Wr1eBtCggHaEK?rs=1&pid=ImgDetMain" width='70%' height='400px' alt="" />
-      </div>
-      <h2>Name:Queen of Tears</h2>
-      <h3>Rating : 8.2</h3>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque minima, officia veritatis doloremque incidunt odit porro, hic harum error fugiat ducimus in? Ducimus minima cum nesciunt distinctio sint numquam ex.</p>
-    </div>
-   </div>
-   <div>
-     <div>
-      <div>
-        <img src="https://th.bing.com/th/id/OIP.q_ncYnMDBTBP8Wr1eBtCggHaEK?rs=1&pid=ImgDetMain" width='70%' height='400px' alt="" />
-      </div>
-      <h2>Name:Queen of Tears</h2>
-      <h3>Rating : 8.2</h3>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque minima, officia veritatis doloremque incidunt odit porro, hic harum error fugiat ducimus in? Ducimus minima cum nesciunt distinctio sint numquam ex.</p>
-    </div>
-   </div>
-   <div>
+    <React.Fragment>
+      <NetflixSeries></NetflixSeries>
     <NetflixSeries></NetflixSeries>
-     <div>
-      <div>
-        <img src="https://th.bing.com/th/id/OIP.q_ncYnMDBTBP8Wr1eBtCggHaEK?rs=1&pid=ImgDetMain" width='70%' height='400px' alt="" />
-      </div>
-      <h2>Name:Queen of Tears</h2>
-      <h3>Rating : 8.2</h3>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque minima, officia veritatis doloremque incidunt odit porro, hic harum error fugiat ducimus in? Ducimus minima cum nesciunt distinctio sint numquam ex.</p>
-    </div>
-   </div>
-   </div>
+    <NetflixSeries></NetflixSeries>
+    <NetflixSeries></NetflixSeries>
+    <NetflixSeries></NetflixSeries>
+    </React.Fragment>
   )
 }
 
 const NetflixSeries=()=>{
+  const returnGenre=()=>{
+    const genre="RomComp";
+    return genre;
+  }
+  const name='Queen of Tears'
+  const rating='8.3'
   return (
     <div>
      <div>
       <div>
         <img src="https://th.bing.com/th/id/OIP.q_ncYnMDBTBP8Wr1eBtCggHaEK?rs=1&pid=ImgDetMain" width='70%' height='400px' alt="" />
       </div>
-      <h2>Name:Queen of Tears</h2>
-      <h3>Rating : 8.2</h3>
+      <h2>Name:{name}</h2>
+      <h3>Rating : {rating }</h3>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque minima, officia veritatis doloremque incidunt odit porro, hic harum error fugiat ducimus in? Ducimus minima cum nesciunt distinctio sint numquam ex.</p>
+      <p>Genre:{returnGenre()}</p>
     </div>
+
    </div>
   )
 }
