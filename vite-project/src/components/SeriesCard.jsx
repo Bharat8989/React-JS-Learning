944417840671b5a13502d9d0c14e896b0b24702f
Bharat <kadamb208@gmail.com>
2024-08-10 10:03:23 +0530

    Inline css styling in react js

--- vite-project/src/components/SeriesCard.jsx
+++ vite-project/src/components/SeriesCard.jsx
@@ -2,20 +2,28 @@ import '../components/Netflix.css'
 const SeriesData=(props)=>{
     console.log(props)
     const {data}=props
+    const btn_style={
+        padding:'1.2rem 2.4rem',
+        border:'none',
+        fontSize:'1.6rem',
+        backgroundColor:'var(--btn-hover-bg-color)',
+        color:'wheat',
+    }
      return(
          
-            <li>
-                <div>
+            <li className='card'>
+                <div className='card-content'>
                   <img className="img-size" src={data.img_url} width="70%" height="70%" alt={data.name} />
-                </div>
+                
                 <h2>Name: {data.name}</h2>
-                <h3>Rating: {data.rating}</h3>
+                <h3 style={{fontSize:'1.3rem'}}>Rating: {data.rating}</h3>
                 <p>Summary: {data.description}</p>
                 <p>Genre: {data.genre}</p>
                 <p>Cast: {data.cast}</p>
                 <a href={data.watch_url} target="_blank" rel="noopener noreferrer">
-                    <button>Watch Now</button>
+                    <button style={btn_style}>Watch Now</button>
                 </a>
+                </div >
             </li>
         );
      
