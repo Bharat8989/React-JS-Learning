c1770e2c409dd4670ecf2bc48665f63330e5c459
Bharat <kadamb208@gmail.com>
2024-08-24 14:01:17 +0530

    adding fiels for props

---
+++ 03tailwinProps/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import Card from './components/Card.jsx'
+
+
+function App() {
+  // const data=[{
+  //   tal:'Delba',
+  //   desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi',
+  //   vive:' View Profile →'
+  // }]
+  
+
+  return (
+    <>
+      {/* <h1 className='text-purple-400/2 bg-green-300 text-black rounded-sm px-3 mb-3'>hello app jsx</h1>
+    
+    <Card userName='king'/>
+    <Card userName='suraj' vive='click me'/> */}
+
+    </>
+  )
+}
+
+export default App
